7930adf5874688123a6864fda3ba915e78796303
Jeshwanth-Gummadavelly <2500032042@kluniversity.in>
2026-08-05 11:20:33 +0530

    EDA (Visualiation Improvements Done)

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
-from src.data.load_data import load_data, get_summary
+from src.data.load_data import load_data
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
 @app.route("/dataset")
 def dataset():
+
     df = load_data()
-    summary = get_summary(df)
-    return render_template("dataset.html", summary=summary)
+
+    return render_template(
+        "load_dataset.html",
+        tables=[df.head().to_html(classes='data', index=False)]
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
